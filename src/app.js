7646790631f04b75ccdaaad1ef71a56f99fbfd62
analeopoldino <analuizagoncalvesleopoldino@gmail.com>
2024-08-30 22:10:43 -0300

    update

---
+++ src/app.js
@@ -0,0 +1,54 @@
+import express from 'express'
+
+const app = express()
+
+// indicar para o express ler body com json
+app.use(express.json())
+
+// mock
+const selecoes = [
+    {id: 1, selecao: 'Brasil', grupo: 'G'},
+    {id: 2, selecao: 'Suiça', grupo: 'G'},
+    {id: 3, selecao: 'Camarões', grupo: 'G'},
+    {id: 4, selecao: 'Servia', grupo: 'G'}
+]
+
+// retornar o objeto por id
+function buscarSelecaoPorId(id){
+    return selecoes.filter( selecao => selecao.id == id )
+}
+
+// pegar a posicao ou index do elemento no array por id
+function buscarIndexSelecao(id){
+    return selecoes.findIndex( selecao => selecao.id == id)
+}
+
+// criar rota padrão ou raiz
+// req = requisição (request), res = resposta (response) 
+// endpopints       
+app.get('/', (req, res) =>{
+    res.send('Hello Word!!')
+})
+
+//status 200 = sucesso
+app.get('/selecoes', (req, res) => {
+    res.status(200).send(selecoes)
+})
+
+app.get('/selecoes/:id', (req, res) => {
+    res.json(buscarSelecaoPorId(req.params.id))
+})
+
+app.post('/selecoes', (req, res) =>{
+    selecoes.push(req.body)
+    res.status(201).send('Seleção cadastrada com sucesso!')
+})
+
+// splice: metodo usado para remover elemento de um array
+app.delete('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes.splice(index, 1)
+res.send(`Seleção com id ${req.params.id} excluída com suceso!`)
+})
+
+export default app
