5e90ce8d70e97bf4294bedb8bc3c171200df43cb
analeopoldino <analuizagoncalvesleopoldino@gmail.com>
2024-09-04 01:31:55 -0300

    Update

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-import app from './app.js';
+import app from './src/app.js';
 
 const PORT = process.env.PORT || 3000;
-const routes = require('./routes');
-
-app.use('/', routes);
 
 //Inicia o servidor na porta especificada.
 app.listen(PORT, () => {
