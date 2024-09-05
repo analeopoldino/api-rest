40369dd5c233938c767b97e763c4a05b67c9036d
analeopoldino <analuizagoncalvesleopoldino@gmail.com>
2024-09-05 01:49:29 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-import app from '../src/app.js'; // ou require('../src/app.js') se estiver sem Babel/ES6
-require('dotenv').config();
+import app from '../src/app.js';
 
 const PORT = process.env.PORT || 3000;
 
-// Inicia o servidor na porta especificada.
+//Inicia o servidor na porta especificada.
 app.listen(PORT, () => {
     console.log(`Servidor rodando no endereço http://localhost:${PORT}`);
 });
+
