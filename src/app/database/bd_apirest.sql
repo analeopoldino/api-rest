0d5d7524051638bbd0eb5a2d9151b325d5321a5e
analeopoldino <analuizagoncalvesleopoldino@gmail.com>
2024-09-05 18:35:23 -0300

    Update

--- src/app/database/bd_apirest.sql
+++ src/app/database/bd_apirest.sql
@@ -1,1 +1,19 @@
-SELECT * FROM bd_apirest.livros;
+-- Cria a tabela apenas se não existir
+CREATE TABLE IF NOT EXISTS livros (
+  id INT NOT NULL AUTO_INCREMENT,
+  titulo VARCHAR(255) NOT NULL,
+  autor VARCHAR(255) NOT NULL,
+  ano VARCHAR(4) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
+
+-- Insere os dados na tabela
+INSERT INTO livros (titulo, autor, ano) VALUES
+('1984', 'George Orwell', '1949'),
+('Dom Quixote', 'Miguel de Cervantes', '1605'),
+('Moby Dick', 'Herman Melville', '1851'),
+('O Grande Gatsby', 'F. Scott Fitzgerald', '1925'),
+('A Revolução dos Bichos', 'George Orwell', '1945'),
+('O Sol é para Todos', 'Harper Lee', '1960'),
+('A Biblioteca da Meia-Noite', 'Matt Haig', '2021'),
+('Dom Casmurro', 'Machado de Assis', '1899');
--- src/app/database/connection.js
+++ src/app/database/connection.js
@@ -1,13 +1,14 @@
-import mysql from 'mysql';
+import mysql from 'mysql2/promise';  // Importa a versão com suporte a Promises
 
 // Configura a conexão com o banco de dados usando variáveis de ambiente ou valores padrão.
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || '3306',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'bd_apirest'
+const connection = await mysql.createConnection({
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT || 3306,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
 });
+
 
 // Conecta ao banco de dados e exibe uma mensagem no console sobre o status da conexão.
 connection.connect((err) => {
