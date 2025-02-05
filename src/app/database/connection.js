61e7ff222e554466958e5de8931ade7049c36fc3
Ana <analuizagoncalvesleopoldino@gmail.com>
2025-02-05 01:35:00 -0300

    Mudando configurações do banco de dados em connection.js

--- src/app/database/connection.js
+++ src/app/database/connection.js
@@ -4,8 +4,8 @@ import mysql from 'mysql';
 const connection = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || '3306',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
+    user: process.env.DB_USER || 'user_api',
+    password: process.env.DB_PASSWORD || 'nova_senha_genérica',
     database: process.env.DB_NAME || 'bd_apirest'
 });
 
